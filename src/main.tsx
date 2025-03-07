00a66d91cdf373c980ca74e61aa870df2c7e03e0
Theodore <79800511+H3LL0Th3Re@users.noreply.github.com>
2025-03-07 10:17:35 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,10 +8,10 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    
       <Provider>
         <App />
       </Provider>
-    </BrowserRouter>
+    
   </StrictMode>
 );
