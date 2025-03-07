3e2f72d5b61758c48baee3b8d193750cb4b29802
Theodore <79800511+H3LL0Th3Re@users.noreply.github.com>
2025-03-07 10:31:04 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths"
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tsconfigPaths from "vite-tsconfig-paths"
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tsconfigPaths()],
+// })
+
+
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      exclude: ['node_modules/', 'src/test/**/*'],
+    },
+  },
+});
