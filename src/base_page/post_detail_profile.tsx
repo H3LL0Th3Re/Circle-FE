a5c8cdb9e910fc09dee6b6b425c57270f9f9d6c1
IamSom3On3 :) <79800511+H3LL0Th3Re@users.noreply.github.com>
2025-03-07 09:38:46 +0700

    work

--- src/base_page/post_detail_profile.tsx
+++ src/base_page/post_detail_profile.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import {
   Box,
   Text,
@@ -96,7 +96,7 @@ function DetailedPostProfile() {
 
   useEffect(() => {
       fetchThreads();
-  }, []);
+  });
 
   
 
