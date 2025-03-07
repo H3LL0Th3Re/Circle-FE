bc6b63fb90b119c6ad98b962b8d326b0a577648c
Theodore <79800511+H3LL0Th3Re@users.noreply.github.com>
2025-03-07 10:21:52 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import { Provider } from "./components/ui/provider.tsx"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
+    <BrowserRouter>
       <Provider>
         <App />
       </Provider>
-    
+    </BrowserRouter>
   </StrictMode>
 );
